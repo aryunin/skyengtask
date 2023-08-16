6ffec24ce9b5ff16bfce3494ec4f9cb2c36fdaea
aryunin <im.aryunin@gmail.com>
2023-08-16 18:04:30 +0300

    Initial commit

---
+++ src/main/java/com/aryunin/skyengtask/SkyengtaskApplication.java
@@ -0,0 +1,13 @@
+package com.aryunin.skyengtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyengtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyengtaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aryunin/skyengtask/SkyengtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryunin.skyengtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyengtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
